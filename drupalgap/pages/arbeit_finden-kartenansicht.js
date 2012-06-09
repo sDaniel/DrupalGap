2d43b6ff08c992eb4873ea6303b7f2b4892fdbda
aajam <abdel25@hotmail.de>
2012-06-09 15:58:17 +0200

    Arbeit anbieten pages
    Arbeit anbieten pages

--- drupalgap/pages/arbeit_finden-kartenansicht.js
+++ drupalgap/pages/arbeit_finden-kartenansicht.js
@@ -1,20 +1,22 @@
-<div data-role="page" id="page1">
-	<div data-role="header">
-  		<a href="dashboard.html" data-icon="home" class="ui-btn-left">Home</a>
-    	<h1>Arbeit finden</h1>
-    	<a href="einstellungen-filter-arbeit_finden.html" data-icon="gear" class="ui-btn-right" id="drupalgap_page_arbeit_finden_button_filtern">Filtern</a>
-  	</div><!-- /header -->
-<!-- /header -->
-    <div data-role="content">
-        2do
-        <div data-role="navbar" data-iconpos="top">
-            <ul>
-                <li>
-                    <a href="arbeit_finden-listenansicht.html" data-theme="c" data-icon="grid">
-                        Listenansicht
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-        </div>
+function checkLocation() {
+var win = function(position) {
+var lat = position.coords.latitude;
+var long = position.coords.longitude;
+var myLatlng = new google.maps.LatLng(lat, long);
+
+var myOptions = {
+center: myLatlng,
+zoom: 16,
+mapTypeId: google.maps.MapTypeId.HYBRID
+};
+map_element = document.getElementById("map_canvas");
+map = new google.maps.Map(map_element, myOptions);
+};
+
+var fail = function(e) {
+$.mobile.hidePageLoadingMsg();
+alert('Can\'t retrieve position.\nError: ' + e);
+};
+
+watchID = navigator.geolocation.getCurrentPosition(win, fail);
+} 
